c2750b63a256ba68893a27de008c21b25188ff17
22120198 <trinhnguyenluong182306@gmail.com>
2025-02-26 20:50:39 +0700

    Modify vue config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,29 +1,28 @@
 const { defineConfig } = require('@vue/cli-service');
-const fs = require('fs');
-const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 
-  devServer: process.env.NODE_ENV === 'development' ? {
-    server: {
-      type: 'https',
-      options: {
-        key: fs.readFileSync(path.join(__dirname, './sslkeys/key.pem')), // Đường dẫn tới private key
-        cert: fs.readFileSync(path.join(__dirname, './sslkeys/cert.pem')), // Đường dẫn tới certificate
-      },
-    },
-    port: 8080,  // Cổng chạy localhost
-    host: 'localhost',
-    allowedHosts: 'all', // Chấp nhận tất cả các hosts
-
+  devServer: {
     proxy: {
       '/api': {
-        target: 'https://website-bookstore.onrender.com', // Backend server
+        target: process.env.NODE_ENV === 'production'
+          ? 'https://website-bookstore.onrender.com' // Backend khi deploy
+          : 'http://localhost:8888', // Backend khi chạy local
         changeOrigin: true,
         pathRewrite: { '^/api': '' },
       },
     },
-  } : undefined, // Không có `devServer` khi chạy trên Vercel
+  },
+
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new (require('webpack')).DefinePlugin({
+          'process.env.VUE_APP_API_URL': JSON.stringify('https://website-bookstore.onrender.com'),
+        })
+      );
+    }
+  },
 });
